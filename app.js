9e809a4f5a1eb9f3148c682a4918c1df0f4cc362
focus9813mr <focus9813mr@gmail.com>
2022-07-24 16:27:59 +0800

    新增 bootstarp 安裝 handlebars

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// 載入 handlebars
+const exphbs = require('express-handlebars')
+// 設定 樣板引擎
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('veiw engine', 'handlebars')
+
 app.get('/', (req, res) => {
   res.send('Hello World~')
 })
